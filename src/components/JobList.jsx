0fadf2c11c7d0fcaddbb938ab2c6f64c6e0abeca
Lacey Murphy <lneagle@gmail.com>
2025-10-08 13:02:21 -0400

    Made table of job data

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,13 +1,34 @@
 function JobList({ jobs }) {
     const jobList = jobs.map(job =>
-        <li key={job.id}><a href={job.url} target="_blank">{job.jobTitle.replace("&amp;#8211;", "–")}</a></li>
+        <tr key={job.id}>
+            <td><a href={job.url} target="_blank">{job.jobTitle.replace("&amp;#8211;", "–").replace("&amp;#038;", "&")}</a></td>
+            <td>{job.companyName.replace("&amp;#038;", "&")}</td>
+            <td>{job.jobIndustry.join(", ").replace("&amp;", "&")}</td>
+            <td>{job.jobType.join(", ")}</td>
+            <td>{job.jobGeo}</td>
+            <td>{job.jobLevel}</td>
+            <td>{job.salaryMin} - {job.salaryMax}{job.salaryCurrency}</td>
+        </tr>
     );
 
 	return (
 		<>
-            <ul>
-                {jobList}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Job Title</th>
+                        <th>Company Name</th>
+                        <th>Industry</th>
+                        <th>Job Type</th>
+                        <th>Location</th>
+                        <th>Job Level</th>
+                        <th>Salary Range</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {jobList}
+                </tbody>
+            </table>
 		</>
 	)
 }

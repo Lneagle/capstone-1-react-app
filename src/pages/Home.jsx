97a650b03218f66ff105bf619184d2bf7e5ddd69
Lacey Murphy <lneagle@gmail.com>
2025-10-10 13:39:13 -0400

    Added homepage text (centered)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ function Home() {
 	return (
 		<>
 			<NavBar />
-			<main>
+			<main className="center">
 				<h1>Search Remote Jobs</h1>
+				<p>Use this app to search for remote jobs by location or industry, or customize your search by industry, location, and keyword.</p>
 				<p>Powered by <a href="https://jobicy.com/">Jobicy</a></p>
 			</main>
 		</>
